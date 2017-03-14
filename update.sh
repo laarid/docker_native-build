f0fc99aa746d757291f88e771d92eefaea96cdba
You-Sheng Yang <vicamo@gmail.com>
2017-03-14 10:18:29 +0800

    fix ppc64el-next build

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ SUITE=stretch
 for f in $(find . -type f -name Dockerfile); do
   [ -n "$(head -n 1 $f | grep '^#.*GENERATED FROM')" ] || continue;
 
-  arch=$(echo "$f" | cut -d/ -f2)
+  arch=$(echo "$f" | cut -d/ -f2 | cut -d- -f1)
   cat Dockerfile.template | \
     sed -e "s,@IMAGE_SUITE@,${SUITE},g" \
       -e "s,@IMAGE_ARCH@,${arch},g" > "$f"
