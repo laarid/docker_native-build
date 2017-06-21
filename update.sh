fe03a627397a479ed4652e3ba0744879855c037d
You-Sheng Yang <vicamo@gmail.com>
2017-06-21 16:58:00 +0800

    flatten folder layout

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # set -x
+SUITE=testing
 
 for f in $(find . -type f -name Dockerfile); do
   [ -n "$(head -n 1 $f | grep '^#.*GENERATED FROM')" ] || continue;
 
-  suite=$(echo "$f" | cut -d/ -f2)
-  arch=$(echo "$f" | cut -d/ -f3 | cut -d- -f1)
+  arch=$(echo "$f" | cut -d/ -f2 | cut -d- -f1)
   cat Dockerfile.template | \
-    sed -e "s,@IMAGE_SUITE@,${suite},g" \
+    sed -e "s,@IMAGE_SUITE@,${SUITE},g" \
       -e "s,@IMAGE_ARCH@,${arch},g" > "$f"
 done
